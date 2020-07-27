f36610befecb0b8c3413dd4bcb49cabf28df6148
A. Jensen <jensen.aaro@gmail.com>
2020-07-27 18:58:16 -0500

    add 007_grant_schema_usage_for_roles.up.sql and 007_grant_schema_usage_for_roles.down.sql

---
+++ migrate/007_grant_schema_usage_for_roles.down.sql
@@ -0,0 +1,4 @@
+REVOKE USAGE ON SCHEMA src FROM stocker_src_rw;
+REVOKE USAGE ON SCHEMA src FROM stocker_src_ro;
+REVOKE USAGE ON SCHEMA report FROM stocker_report_rw;
+REVOKE USAGE ON SCHEMA report FROM stocker_report_ro;
---
+++ migrate/007_grant_schema_usage_for_roles.up.sql
@@ -0,0 +1,4 @@
+GRANT USAGE ON SCHEMA src TO stocker_src_rw;
+GRANT USAGE ON SCHEMA src TO stocker_src_ro;
+GRANT USAGE ON SCHEMA report TO stocker_report_rw;
+GRANT USAGE ON SCHEMA report TO stocker_report_ro;
