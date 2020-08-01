e20d2e972ebc158f9b8351123770a67e6acd0109
A. Jensen <jensen.aaro@gmail.com>
2020-07-31 19:49:36 -0500

    add created and modified columns

---
+++ migrate/008_add_created_and_modified_columns.down.sql
@@ -0,0 +1,14 @@
+alter table src."Candles"
+    drop "Created",
+    drop "Modified"
+;
+
+alter table src."CompanyProfile"
+    drop "Created",
+    drop "Modified"
+;
+
+alter table src."Stocks"
+    drop "Created",
+    drop "Modified"
+;
