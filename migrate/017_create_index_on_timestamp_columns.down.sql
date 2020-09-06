f8ca984b076a3a02e910e5b301868566bf5c997b
A. Jensen <jensen.aaro@gmail.com>
2020-09-06 10:54:48 -0500

    add indexes on the modified columns in src and stage

---
+++ migrate/017_create_index_on_timestamp_columns.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX src.ndx_candles_modified;
+DROP INDEX stage.ndx_candles_modified;
---
+++ migrate/017_create_index_on_timestamp_columns.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX ndx_candles_modified ON src."Candles"("Modified");
+CREATE INDEX ndx_candles_modified ON stage."Candles"("Modified");
